996e110319a27f5ecc4309c37720ffb1a16abf47
kimx <kimxinfo@gmail.com>
2015-10-29 15:28:04 +0800

    2

--- RSACryptoLab/Program.cs
+++ RSACryptoLab/Program.cs
@@ -7,6 +7,7 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
+//http://codebetter.com/johnvpetersen/2012/04/02/making-your-asp-net-web-apis-secure/
 namespace RSACryptoLab
 {
     //class Program
